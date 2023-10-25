c5f2442dd30fa24f49a9c05f4df1b36485f7d334
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2023-10-25 12:21:51 +0200

    #8 reservation can be done

--- code/frontend/js/app.js
+++ code/frontend/js/app.js
@@ -1,3 +1,4 @@
+// popup window
 document.addEventListener("DOMContentLoaded", function () {
     var openPopupButton = document.getElementById("openPopupButton");
     var modal = document.getElementById("myModal");
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
-// main.ts
+// 
 document.addEventListener("DOMContentLoaded", function () {
     // Function to assign unique IDs to the columns
     function assignColumnIds() {
@@ -30,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 for (var j = 1; j < rows.length; j++) {
                     var cell = rows[j].children[i];
                     // Generate a unique ID based on the column index and row index
-                    cell.id = "cell_".concat(i, "_").concat(j);
+                    cell.id = "cell_".concat(j, "_").concat(i);
                 }
             }
         }
@@ -38,21 +39,75 @@ document.addEventListener("DOMContentLoaded", function () {
     // Call the function to assign IDs
     assignColumnIds();
 });
-// script.ts
-// script.ts
+// get all values from dropdown
 document.addEventListener("DOMContentLoaded", function () {
-    var dropdownD = document.getElementById("day");
-    var dropdown = document.getElementById("time");
-    var dropdownE = document.getElementById("timeE");
+    var dropdownDay = document.getElementById("day");
+    var dropdownStartTime = document.getElementById("time");
+    var dropdownEndTime = document.getElementById("timeE");
     var submitButton = document.getElementById("submitButton");
     submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener("click", function () {
-        var selectedValue = dropdown.value;
-        var selectedValueE = dropdownE.value;
-        var selectedValueD = dropdownD.value;
-        if (selectedValue) {
-            console.log(selectedValue);
-            console.log(selectedValueE);
-            console.log(selectedValueD);
+        // get values from dropdown
+        var startTime = dropdownStartTime.value;
+        var endTime = dropdownEndTime.value;
+        var day = dropdownDay.value;
+        // ceck if all set
+        if (startTime && endTime && day) {
+            if (endTime > startTime) {
+                // get values/convert to id/ make reservation
+                getColumn(startTime, endTime, day);
+            }
         }
     });
 });
+// adding color to box
+function addReservation(array) {
+    for (var i = 0; i < array.length; i++) {
+        var id = document.getElementById(array[i]);
+        if (id) {
+            id.style.backgroundColor = "#eb4258";
+        }
+    }
+}
+// convert ids to the cell id
+function toCell(startTimeId, dayId) {
+    return "cell_".concat(startTimeId, "_").concat(dayId);
+}
+// get assigned columns
+function getColumn(startTime, endTime, day) {
+    // arrays with the values
+    var startTimeArray = ["07:00", "08:00", "08:55", "10:00", "10:55", "11:50", "12:45", "13:40", "14:35", "15:30", "16:25", "17:20", "18:15", "19:10", "20:05", "21:00", "21:55"];
+    var endTimeArray = ["07:50", "08:50", "09:45", "10:50", "11:45", "12:40", "13:35", "14:30", "15:25", "16:20", "17:15", "18:10", "19:05", "20:00", "20:50", "21:45", "22:40"];
+    var dayArray = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"];
+    // importatnt variables
+    var dayId = 0; // id of day
+    var units = 0; // count uf units reservated
+    var startTimeId = 0; // reservation start time
+    var columnIds = []; // array of all ids
+    // get start id
+    for (var i = 0; i < startTimeArray.length; i++) {
+        if (startTimeArray[i] === startTime) {
+            startTimeId = i;
+            // get units
+            for (var j = i; j < endTimeArray.length; j++) {
+                units++;
+                if (endTimeArray[j] === endTime) {
+                    break;
+                }
+            }
+            ;
+        }
+    }
+    ;
+    // get dayId
+    for (var i = 0; i < dayArray.length; i++) {
+        if (dayArray[i] === day) {
+            dayId = i + 1;
+        }
+    }
+    // fill array with the ids from html
+    for (var i = startTimeId; i < startTimeId + units; i++) {
+        columnIds.push(toCell(i + 1, dayId));
+    }
+    // add reservations to calendar
+    addReservation(columnIds);
+}
--- code/frontend/js/app.ts
+++ code/frontend/js/app.ts
@@ -1,3 +1,4 @@
+// popup window
 document.addEventListener("DOMContentLoaded", () => {
     const openPopupButton = document.getElementById("openPopupButton") as HTMLButtonElement;
     const modal = document.getElementById("myModal") as HTMLDivElement;
@@ -22,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// main.ts
+// 
 document.addEventListener("DOMContentLoaded", () => {
     // Function to assign unique IDs to the columns
     function assignColumnIds() {
@@ -36,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 for (let j = 1; j < rows.length; j++) {
                     const cell = (rows[j].children[i] as HTMLTableCellElement);
                     // Generate a unique ID based on the column index and row index
-                    cell.id = `cell_${i}_${j}`;
+                    cell.id = `cell_${j}_${i}`;
                 }
             }
         }
@@ -46,23 +47,88 @@ document.addEventListener("DOMContentLoaded", () => {
     assignColumnIds();
 });
 
-// script.ts
-// script.ts
+// get all values from dropdown
 document.addEventListener("DOMContentLoaded", () => {
-    const dropdownD = document.getElementById("day") as HTMLSelectElement;
-    const dropdown = document.getElementById("time") as HTMLSelectElement;
-    const dropdownE = document.getElementById("timeE") as HTMLSelectElement;
+    const dropdownDay = document.getElementById("day") as HTMLSelectElement;
+    const dropdownStartTime = document.getElementById("time") as HTMLSelectElement;
+    const dropdownEndTime = document.getElementById("timeE") as HTMLSelectElement;
     const submitButton = document.getElementById("submitButton");
 
     submitButton?.addEventListener("click", () => {
-        const selectedValue = dropdown.value;
-        const selectedValueE = dropdownE.value;
-        const selectedValueD = dropdownD.value;
+        // get values from dropdown
+        const startTime = dropdownStartTime.value;
+        const endTime = dropdownEndTime.value;
+        const day = dropdownDay.value;
 
-        if (selectedValue) {
-            console.log(selectedValue);
-            console.log(selectedValueE);
-            console.log(selectedValueD);
+        // ceck if all set
+        if (startTime && endTime && day) {
+            if (endTime > startTime) {
+                // get values/convert to id/ make reservation
+                getColumn(startTime, endTime, day);
+            }
         }
     });
 });
+
+// adding color to box
+function addReservation(array: string[]) {
+    for (let i: number = 0; i < array.length; i++) {
+        let id = document.getElementById(array[i]);
+        if (id) {
+            id.style.backgroundColor = "#eb4258";
+        }
+    }
+}
+
+
+// convert ids to the cell id
+function toCell(startTimeId: number, dayId: number): string {
+    return `cell_${startTimeId}_${dayId}`;
+}
+
+// get assigned columns
+function getColumn(startTime: string, endTime: string, day: string) {//: String[] {
+
+    // arrays with the values
+    const startTimeArray: string[] = ["07:00", "08:00", "08:55", "10:00", "10:55", "11:50", "12:45", "13:40", "14:35", "15:30", "16:25", "17:20", "18:15", "19:10", "20:05", "21:00", "21:55"];
+    const endTimeArray: string[] = ["07:50", "08:50", "09:45", "10:50", "11:45", "12:40", "13:35", "14:30", "15:25", "16:20", "17:15", "18:10", "19:05", "20:00", "20:50", "21:45", "22:40"];
+    const dayArray: string[] = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"];
+
+    // importatnt variables
+    let dayId: number = 0; // id of day
+    let units: number = 0; // count uf units reservated
+    let startTimeId: number = 0; // reservation start time
+    let columnIds: string[] = []; // array of all ids
+
+    // get start id
+    for (let i: number = 0; i < startTimeArray.length; i++) {
+        if (startTimeArray[i] === startTime) {
+            startTimeId = i;
+
+            // get units
+            for (let j: number = i; j < endTimeArray.length; j++) {
+                units++;
+                if (endTimeArray[j] === endTime) {
+                    break;
+                }
+            };
+        }
+    };
+
+    // get dayId
+    for (let i: number = 0; i < dayArray.length; i++) {
+        if (dayArray[i] === day) {
+            // + 1 because the first column is 1 not 0
+            dayId = i + 1;
+        }
+    }
+
+    // fill array with the ids from html
+    for (let i: number = startTimeId; i < startTimeId + units; i++) {
+        // + 1 because the first row is 1 not 0
+        columnIds.push(toCell(i + 1, dayId));
+    }
+
+    // add reservations to calendar
+    addReservation(columnIds);
+}
