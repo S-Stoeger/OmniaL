e18cbb3f3a24de599dd5dfa85df9a1af6e08a408
Sophie-Binder <sophie.anja.binder@gmx.at>
2023-11-17 09:28:28 +0100

    updatet Test

--- code/backend/omnial/src/test/java/at/htlleonding/omnial/TestReservations.java
+++ code/backend/omnial/src/test/java/at/htlleonding/omnial/TestReservations.java
@@ -13,16 +13,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestReservations {
 
+    private static ReservationRepository repository = new ReservationRepository();
     @BeforeAll
     static void addTestReservation(){
-        ReservationRepository repository = new ReservationRepository();
         Reservation reservation = new Reservation(1,1, LocalDateTime.of(2023,11,15,15, 00,00), LocalDateTime.of(2023,11,15,20, 50,00), LocalDate.of(2023,11,15));
         repository.addReservation(reservation);
     }
     @Test
     void testReadFromFile() {
-        ReservationRepository repository = new ReservationRepository();
-
         String text = repository.getReservationsFromFile();
 
         System.out.println(text);
@@ -30,8 +28,6 @@ public class TestReservations {
 
     @Test
     void TestGetAllReservations() {
-        ReservationRepository repository = new ReservationRepository();
-
         List<Reservation> reservations = repository.getAllReservation();
 
         System.out.println(reservations);
@@ -39,7 +35,6 @@ public class TestReservations {
 
     @Test
     void TestAddReservation() {
-        ReservationRepository repository = new ReservationRepository();
         Reservation reservation = new Reservation(1,1, LocalDateTime.of(2023,11,15,12, 00,00), LocalDateTime.of(2023,11,15,12, 50,00), LocalDate.of(2023,11,15));
         repository.addReservation(reservation);
 
@@ -47,7 +42,6 @@ public class TestReservations {
 
     @Test
     void TestAddIllegalEndTime(){
-        ReservationRepository repository = new ReservationRepository();
         Reservation illegalReservation = new Reservation(1,1, LocalDateTime.of(2023,11,15,9, 00,00), LocalDateTime.of(2023,11,15,10, 30,00), LocalDate.of(2023,11,15));
 
         assertThrows(IllegalArgumentException.class, () -> {
@@ -58,7 +52,6 @@ public class TestReservations {
 
     @Test
     void TestAddIllegalStarTime(){
-        ReservationRepository repository = new ReservationRepository();
         Reservation illegalReservation = new Reservation(1,1, LocalDateTime.of(2023,11,15,16, 00,00), LocalDateTime.of(2023,11,15,21, 30,00), LocalDate.of(2023,11,15));
 
         assertThrows(IllegalArgumentException.class, () -> {
@@ -71,7 +64,6 @@ public class TestReservations {
 
     @Test
     void TestAddIllegalTime(){
-        ReservationRepository repository = new ReservationRepository();
         Reservation illegalReservation = new Reservation(1,1, LocalDateTime.of(2023,11,15,17, 00,00), LocalDateTime.of(2023,11,15,17, 30,00), LocalDate.of(2023,11,15));
 
         assertThrows(IllegalArgumentException.class, () -> {
