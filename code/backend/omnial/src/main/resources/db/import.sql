38cb1c2c6c4dfe042d6b862e8459122daaed6cd6
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2023-12-03 18:44:45 +0100

    bugfix: changed starttime to valid starttime

--- code/backend/omnial/src/main/resources/db/import.sql
+++ code/backend/omnial/src/main/resources/db/import.sql
@@ -5,4 +5,4 @@ insert into Person
 values (nextval('person_seq'),'admin', 'admin');
 
 insert into reservation
-values (nextval('reservation_seq'), 1, '2023-11-15',1,'2023-11-15T20:00:00','2023-11-15T10:50:00');
+values (nextval('reservation_seq'), 1, '2023-11-15',1,'2023-11-15T20:00:00','2023-11-15T10:55:00');
