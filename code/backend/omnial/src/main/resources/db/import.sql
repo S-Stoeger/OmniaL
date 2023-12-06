32f18a79050e3c08d6c6d06d352d3a35c1e35b63
S-Stoeger <sost-1@gmx.at>
2023-12-06 12:32:48 +0100

    changed test values in db

--- code/backend/omnial/src/main/resources/db/import.sql
+++ code/backend/omnial/src/main/resources/db/import.sql
@@ -2,7 +2,7 @@ insert into Room
 values (nextval('room_seq'),'Das Fotostudio steht für Schüler der Medientechnik zur verfügung um an Foto- und Videoprojekten zu arbeiten','Fotostudio');
 
 insert into Person
-values (nextval('person_seq'),'admin', 'admin');
+values (nextval('person_seq'),'teacher', 'teacher', 'teacher', 'teacher.teacher@gmail.com');
 
 insert into reservation
 values (nextval('reservation_seq'), 1, '2023-11-15',1,'2023-11-15T20:00:00','2023-11-15T10:55:00');
