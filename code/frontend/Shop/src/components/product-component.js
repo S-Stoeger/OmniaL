030912a0beaa7632c360065b871612b4aba57c62
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2024-10-24 23:12:45 +0200

    added sampleproducts and filter (non functional)

---
+++ code/frontend/Shop/src/components/product-component.js
@@ -0,0 +1,26 @@
+function printProductRows() {
+    const products = document.getElementById("products");
+    let resultstring = "";
+
+    for (let i = 0; i < 3; i++) {
+        resultstring += `<div class="product-row">`;
+
+        for (let j = 0; j < 4; j++) {
+            resultstring += `
+            <div class="product">
+                <p>Titel</p>
+                <div>
+                    <p>Full name</p>
+                    <p>Type</p>
+                    <img>
+                </div>
+            </div>`;
+        }
+
+        resultstring += `</div>`;
+    }
+    
+    if (products != null)
+        products.innerHTML = resultstring;
+}
+printProductRows();
