ab7eab7451bda99c971104ec9fda11aa86534a32
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2024-12-17 16:49:16 +0100

    #163 added dasboard

---
+++ code/frontend/src/app/shop-dashboard/shop-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-dashboard',
+  imports: [],
+  templateUrl: './shop-dashboard.component.html',
+  styleUrl: './shop-dashboard.component.css'
+})
+export class ShopDashboardComponent {
+
+}
