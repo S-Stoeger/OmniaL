76cdae3a90f4d4ab0d6790caefc5e81be607a76d
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2024-12-17 16:44:15 +0100

    #162 added startpage + fiexed image path

--- code/frontend/src/app/app.routes.ts
+++ code/frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {ShopStartComponent} from './shop-start/shop-start.component';
+import {ShopDashboardComponent} from './shop-dashboard/shop-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShopStartComponent,
+    title: 'Start'
+  },
+  {
+    path: 'dashboard',
+    component: ShopDashboardComponent,
+    title: 'Dashboard'
+  }
+];
---
+++ code/frontend/src/app/shop-start/shop-start.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-shop-start',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './shop-start.component.html',
+  styleUrl: './shop-start.component.css'
+})
+export class ShopStartComponent {
+
+}
