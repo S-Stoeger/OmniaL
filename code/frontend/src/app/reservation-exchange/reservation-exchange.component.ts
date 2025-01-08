2d0b0701ebee7df8b027ca56d4c244074d45f7ea
MaxaxSlabschi <m.slabschi@students.htl-leonding.ac.at>
2025-01-08 14:11:47 +0100

    #173 added modal for dashboard

--- code/frontend/src/app/reservation-exchange/reservation-exchange.component.ts
+++ code/frontend/src/app/reservation-exchange/reservation-exchange.component.ts
@@ -1,20 +1,40 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Reservation} from '../reservation';
-import {DatePipe, NgForOf} from '@angular/common';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-reservation-exchange',
   imports: [
     NgForOf,
-    DatePipe
+    DatePipe,
+    NgIf
   ],
   templateUrl: './reservation-exchange.component.html',
   styleUrl: './reservation-exchange.component.css'
 })
 export class ReservationExchangeComponent {
   @Input() reservations!: Reservation[];
+  @Output() close = new EventEmitter<void>();
+  isModalOpen: boolean = false;
+  modalReservation: Reservation | null = null;
 
   constructor() {
     console.log(this.reservations)
+  }
+
+  openModal(reservation: Reservation) {
+    this.isModalOpen = true;
+    this.modalReservation = reservation;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.close.emit();
+  }
+
+  onBackgroundClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+      this.closeModal();
+    }
   }
 }
